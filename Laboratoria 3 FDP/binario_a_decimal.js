7332bb3ed5e6276b02e12006885c943967a70c8a
José Moisés Martínez Hernández <jose.martinez@ulv.edu.mx>
2024-11-23 20:12:51 -0600

    Verificacion del numero binario y comentarios

--- Laboratoria 3 FDP/binario_a_decimal.js
+++ Laboratoria 3 FDP/binario_a_decimal.js
@@ -5,10 +5,23 @@ let e = bnar.length; // Longitud del num binario
 let n = 0; // Posicion del digito
 let d = 0; // Valor en decimal
 
-while(n<e){
-    let ud = parseInt(bnar[e - 1 - n]); 
-    d = d + (ud*2**n)
-    n++
+//Condicion para saber si lo ingresado por el usuario es valido.
+//se busca si el arreglo contiene los digitos 1 o 0 que son los validos.
+if (bnar.some(digito => digito !== '0' && digito !== '1')){
+    //si no es un binario, se manda un mesaje que se ingrese un numero valido.
+    console.log("Ingresa un número binario válido (solo 0 y 1).");
+}else{
+    //mientras la posicion es menor a la longitud del array
+    while(n<e){
+        //se recorre cada digito de derecha a izquierda
+        let ud = parseInt(bnar[e - 1 - n]);
+        //al ultimo digito se multiplica por 2 a la posicion
+        d = d + (ud*2**n)
+        //se incrementa la posicion del digito
+        n++
+    }
+    //se muestra el resultado
+    console.log(`El valor del binario en decimal es: ${d}`);
 }
 
-console.log(d);
+//Fin
